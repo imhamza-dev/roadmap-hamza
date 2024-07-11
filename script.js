a7a0a0e9b5ce53ef28b783b434f005087729befd
Mr Hamza <imhamza.dev@gmail.com>
2024-07-11 23:34:25 +0500

    rowy-navigation-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyDarWHUzlTBfaWoy0j18S0waQUOZpkuflE',
+	authDomain: 'crudapp-91890.firebaseapp.com',
+	databaseURL: 'https://crudapp-91890-default-rtdb.firebaseio.com',
+	projectId: 'crudapp-91890',
+	storageBucket: 'crudapp-91890.appspot.com',
+	messagingSenderId: '302876566729',
+	appId: '1:302876566729:web:9b20277cd3aee7e5d2d918',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
