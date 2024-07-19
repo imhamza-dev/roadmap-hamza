51d8dad6e8231301399b6504fc03dc2c6fbf4e32
Mr Hamza <imhamza.dev@gmail.com>
2024-07-19 21:47:38 +0500

    update

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-	},
-};
-
 import './style.css';
 
 // realtime-database
+
+document
+	.getElementById('statusForm')
+	.addEventListener('submit', function (event) {
+		event.preventDefault();
+
+		// Get the form values
+		const status = document.getElementById('status').value;
+		const title = document.getElementById('title').value;
+		const description = document.getElementById('description').value;
+
+		// Display the submitted content
+		document.getElementById('displayStatus').textContent = status;
+		document.getElementById('displayTitle').textContent = title;
+		document.getElementById('displayDescription').textContent = description;
+
+		// Hide the form and show
+
+		// document.getElementById('statusForm').style.display = 'none';
+		document.getElementById('contentDisplay').classList.remove('hidden');
+	});
