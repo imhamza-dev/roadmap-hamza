78717af5d25d93c261055c7fab9b4b8f93c8c49e
Mr Hamza <imhamza.dev@gmail.com>
2024-07-24 14:38:13 +0500

    ngrok-testing

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ import './style.css';
 
 // realtime-database
 
-document
-	.getElementById('statusForm')
-	.addEventListener('submit', function (event) {
-		event.preventDefault();
+// document
+// 	.getElementById('statusForm')
+// 	.addEventListener('submit', function (event) {
+// 		event.preventDefault();
 
-		// Get the form values
-		const status = document.getElementById('status').value;
-		const title = document.getElementById('title').value;
-		const description = document.getElementById('description').value;
+// 		// Get the form values
+// 		const status = document.getElementById('status').value;
+// 		const title = document.getElementById('title').value;
+// 		const description = document.getElementById('description').value;
 
-		// Display the submitted content
-		document.getElementById('displayStatus').textContent = status;
-		document.getElementById('displayTitle').textContent = title;
-		document.getElementById('displayDescription').textContent = description;
+// 		// Display the submitted content
+// 		document.getElementById('displayStatus').textContent = status;
+// 		document.getElementById('displayTitle').textContent = title;
+// 		document.getElementById('displayDescription').textContent = description;
 
-		// Hide the form and show
+// 		// Hide the form and show
 
-		// document.getElementById('statusForm').style.display = 'none';
-		document.getElementById('contentDisplay').classList.remove('hidden');
-	});
+// 		// document.getElementById('statusForm').style.display = 'none';
+// 		document.getElementById('contentDisplay').classList.remove('hidden');
+// 	});
